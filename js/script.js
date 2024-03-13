b135a357ff26d6e56373142edd2facbdf429ee53
Anna Powaska <annapowaska@op.pl>
2024-03-13 07:45:05 +0100

    Code cleanup

--- js/script.js
+++ js/script.js
@@ -6,24 +6,21 @@ function clickHandler(event) {
     const clickedElement = this;
     const activeLinks = document.querySelectorAll('.list a.active');
     const activeGalleries = document.querySelectorAll('.post')
-    
-   const linkAttribute = clickedElement.getAttribute('href');
-  
-
-   const targetGallery = document.querySelector(linkAttribute)
+    const linkAttribute = clickedElement.getAttribute('href');
+    const targetGallery = document.querySelector(linkAttribute)
+    const links = document.querySelectorAll('.titles a');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active')
     }
 
-     clickedElement.classList.add('active')
+    clickedElement.classList.add('active')
 
     for(let activeGallery of activeGalleries){
         activeGallery.classList.remove('active')
     }
   
     targetGallery.classList.add('active')
-
 }
 
 const links = document.querySelectorAll('.titles a');
