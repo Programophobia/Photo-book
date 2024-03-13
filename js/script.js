2cb73f429d41824fa838549d96f36f71bef2c6e7
Anna Powaska <annapowaska@op.pl>
2024-03-13 07:39:06 +0100

    Add active gallery by clicked country links

--- js/script.js
+++ js/script.js
@@ -1,27 +1,30 @@
 'use strict';
 
-function clickHandler() {
-    console.log(' klikniety')
+function clickHandler(event) {
 
-
-  /* remove class 'active' from all article links  */
-
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
-
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
-
-
+    event.preventDefault();
+    const clickedElement = this;
+    const activeLinks = document.querySelectorAll('.list a.active');
+    const activeGalleries = document.querySelectorAll('.post')
+    
+   const linkAttribute = clickedElement.getAttribute('href');
   
+
+   const targetGallery = document.querySelector(linkAttribute)
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active')
+    }
+
+     clickedElement.classList.add('active')
+
+    for(let activeGallery of activeGalleries){
+        activeGallery.classList.remove('active')
+    }
+  
+    targetGallery.classList.add('active')
+
 }
-
-
-
 
 const links = document.querySelectorAll('.titles a');
 
