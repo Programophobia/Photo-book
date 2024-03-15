20b325c0553a879631359dea1b43b458698ce52e
Anna Powaska <annapowaska@op.pl>
2024-03-15 09:32:20 +0100

    Add generate titles by id in JS

--- js/script.js
+++ js/script.js
@@ -23,8 +23,62 @@ function clickHandler(event) {
     targetGallery.classList.add('active')
 }
 
-const links = document.querySelectorAll('.titles a');
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
-for(let link of links){
-    link.addEventListener('click', clickHandler)
+function generateTitleLinks(){
+
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+
+    for(let article of articles){
+        const articleId = article.getAttribute('id')
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+        const linkHTML = '<li><a class="list-group-item list-group-item-action list-group-item-secondary" href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        html = html + linkHTML
+        titleList.innerHTML = html;   
+    }
+
+    const links = document.querySelectorAll('.titles a');
+    console.log(links)
+
+    for(let link of links){
+        link.addEventListener('click', clickHandler)
+    }
 }
+generateTitleLinks();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
