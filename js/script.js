1c758113a0801dc097bfc79e0ccfff0df25c7c70
Anna Powaska <annapowaska@op.pl>
2024-03-17 06:55:49 +0100

    Link added

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ function clickHandler(event) {
     event.preventDefault();
     const clickedElement = this;
     const activeLinks = document.querySelectorAll('.list a.active');
-    const activeGalleries = document.querySelectorAll('.post')
+    const activeGalleries = document.querySelectorAll(optArticleSelector)
     const linkAttribute = clickedElement.getAttribute('href');
     const targetGallery = document.querySelector(linkAttribute)
-    const links = document.querySelectorAll('.titles a');
+    const links = document.querySelectorAll(optTitleListSelector, 'a');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active')
@@ -19,20 +19,19 @@ function clickHandler(event) {
     for(let activeGallery of activeGalleries){
         activeGallery.classList.remove('active')
     }
-  
     targetGallery.classList.add('active')
 }
 
 const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
 function generateTitleLinks(){
 
-  const titleList = document.querySelector(optTitleListSelector);
-  titleList.innerHTML = '';
-  const articles = document.querySelectorAll(optArticleSelector);
-  let html = '';
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
 
     for(let article of articles){
         const articleId = article.getAttribute('id')
@@ -53,20 +52,17 @@ function generateTitleLinks(){
 generateTitleLinks();
 
 
+function printAbout(msg) {
+    const paragrapf = document.createElement('a');
+    paragrapf.innerHTML = msg;
+    paragrapf.classList.add('link-Bolivia')
+    document.getElementById('box-show').appendChild(paragrapf)
+}
 
+printAbout('more here')
 
-
-
-
-
-
-
-
-
-
-
-
-
+ const linkBolivia = document.querySelector('.link-Bolivia')
+ linkBolivia.setAttribute("href", "https://annapowaska.com/blog/bolivia/")
 
 
 
